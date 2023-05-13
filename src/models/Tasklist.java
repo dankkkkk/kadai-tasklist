6695692430854cc08081e62b719120535aa307db
kato.daisuke3 <d_kato758@jtb.com>
2023-05-13 17:24:01 +0900

    フィルタ修正。完成。

--- src/models/Tasklist.java
+++ src/models/Tasklist.java
@@ -24,7 +24,7 @@ public class Tasklist {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "content", length = 255, nullable = false)
+    @Column(name = "content", length = 2000, nullable = false)
     private String content;
 
     @Column(name = "created_at", nullable = false)
